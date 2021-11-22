2558043cf2eae2256516d7eba1f28bb74a869899
Rahul Behera <rahulbehera2011@gmail.com>
2021-11-23 00:55:17 +0530

    sandbox ts and js

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var dev = 'Rahul';
+console.log(dev);
+var list = document.querySelectorAll('input');
+console.log(list);
+list.forEach(function (element) {
+    console.log(element);
+});
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+const dev = 'Rahul'
+
+console.log(dev);
+
+const list = document.querySelectorAll('input')
+
+console.log(list);
+
+list.forEach(element => {
+    console.log(element);
+    
+});
